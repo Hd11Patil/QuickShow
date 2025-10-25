177ed179d6e63553a1b6b6085ee9144e21ca8364
Hd11Patil <harshal.dp11@gmail.com>
2025-10-25 17:00:58 +0530

    jj

--- client/src/pages/MyBooking.jsx
+++ client/src/pages/MyBooking.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const MyBooking = () => {
-  return <div></div>;
+  const currency = import.meta.env.VITE_CURRENCY;
+
+  const [bookings, setBookings] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getMyBookings = async () => {
+    setBookings(dummyBookingData);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getMyBookings();
+  }, []);
+
+  return !isLoading ? <div></div> : <Loading />;
 };
 
 export default MyBooking;
