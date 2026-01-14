c577c5e8442c20bd19b788acd207912146e8f195
Hd11Patil <harshal.dp11@gmail.com>
2026-01-14 22:29:28 +0530

    MovieDataAdd

--- client/src/pages/admin/Layout.jsx
+++ client/src/pages/admin/Layout.jsx
@@ -3,8 +3,8 @@ import AdminNavbar from "../../components/admin/AdminNavbar";
 import AdminSidebar from "../../components/admin/AdminSidebar";
 import { Outlet } from "react-router-dom";
 import { useAppContext } from "../../context/AppContext";
-
 import Loading from "../../components/Loading";
+import { useEffect } from "react";
 
 const Layout = () => {
   const { isAdmin, fetchIsAdmin } = useAppContext();
