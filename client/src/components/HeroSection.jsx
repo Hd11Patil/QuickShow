486a62869862c6aacb0a7c4d252447e196e62c9e
Harshal Patil <harshal.dp11@gmail.com>
2026-04-23 22:30:23 +0530

    Final UI Updated

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -10,25 +10,19 @@ const HeroSection = () => {
     <div className='flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-[url("/backgroundImage.png")] bg-cover bg-center h-screen'>
       <img src={assets.marvelLogo} alt="" className="max-h-11 lg:h-11 mt-20" />
 
-      <h1 className="text-5xl md:text-[70px] md:leading-18 font-semibold max-w-110">
-        Guardians <br /> of the Galaxy
+      <h1 className="text-5xl md:text-[70px] md:leading-18 font-semibold max-w-120">
+        Book Movie <br /> Tickets Online
       </h1>
 
       <div className="flex items-center gap-4 text-gray-300">
-        <span>Action | Adventure | Sci-Fi</span>
+        <span>Action | Adventure | Comedy | Drama | Horror</span>
+        
 
-        <div className="flex items-center gap-1">
-          <CalendarIcon className="w-4.5 h-4.5" /> 2018
-        </div>
-
-        <div className="flex items-center gap-1">
-          <ClockIcon className="w-4.5 h-4.5" /> 2h 8m
-        </div>
       </div>
 
       <p className="max-w-md text-gray-300">
-        In a post-apocalyptic worls where cities ride on wheels and each other
-        to survive,two people meet in London and try to sleep a conspiracy.
+        Your Movie Experience Starts Here
+Discover, choose, and book your perfect seats effortlessly.
       </p>
       <button
         onClick={() => navigate("/movies")}
